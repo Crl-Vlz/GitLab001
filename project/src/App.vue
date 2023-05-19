c462c3b92d8269987b1be38bf0058680059534ee
Crl-Vlz <crlvlz0215@gmail.com>
2023-05-19 11:30:39 -0600

    Started work on monitoring

--- project/src/App.vue
+++ project/src/App.vue
@@ -6,4 +6,22 @@
 
 <script setup>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, "test@mail.com", "password01")
+  .then((userCrendential) => {
+    const user = userCrendential.user;
+    if (user == null){
+      console.log("Not authentificated")
+    } else {
+      console.log("User verified")
+    }
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    console.log(errorCode, errorMessage)
+  });
+
 </script>
--- project/src/views/Tab2Page.vue
+++ project/src/views/Tab2Page.vue
@@ -2,17 +2,17 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Monitoreo de usuarios</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Monitoreo de usuarios</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+      <ExploreContainer name="Monitoreo de usuarios" />
     </ion-content>
   </ion-page>
 </template>
